9dff35c0a2fafe69e473d68fb74cf04d49b6696b
Leonardo do Amaral Quinquio <leonarddoamaral@gmail.com>
2025-08-29 11:36:33 -0300

    aula 29-08 Tem que arrumar

--- aula1-2/lembretes/index.js
+++ aula1-2/lembretes/index.js
@@ -1,23 +1,32 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require("axios");
+
 app.use(bodyParser.json());
 lembretes = {};
 contador = 0;
 
-app.get ('/lembretes', (req, res) => {
+app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 
 });
-app.put('/lembretes', (req, res) => {
-contador++;
-const { texto } = req.body;
-lembretes[contador] = {
-contador, texto
- }
-res.status(201).send(lembretes[contador]);
+app.put('/lembretes', async (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
 });
 
 app.listen(4000, () => {
-console.log('Lembretes. Porta 4000');
+    console.log('Lembretes. Porta 4000');
 });
+
+
